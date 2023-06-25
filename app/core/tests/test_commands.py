3c4416784bd9de8c8f12739518e92ef382ecedf8
seymaozler <seymaaozlerr@gmail.com>
2023-06-25 14:34:29 +0300

    fixed error

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -21,4 +21,3 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-        
