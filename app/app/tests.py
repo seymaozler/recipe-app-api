3f3a8f691681ebdefa097b237e2d2a594eb77379
seymaozler <seymaaozlerr@gmail.com>
2023-06-25 14:44:54 +0300

    fixed error

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,7 +7,7 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(3, 8)
         self.assertEqual(res, 11)
-    
     def test_subtract_numbers(self):
         res = calc.subtract(11, 5)
         self.assertEqual(res, 6)
+
